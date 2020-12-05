8d5fd1ac33b90ca8aaa3078b4dfcd4446e20a19c
yogesh gawali <yogeshgawali27@gmail.com>
2020-12-05 12:57:46 +0530

    addition 5th dec 20 0

--- Pattern/landNumbersFromPlot1.c
+++ Pattern/landNumbersFromPlot1.c
@@ -5,6 +5,7 @@ char plot[MAX_SIZE][MAX_SIZE];
 int row, col;
 void modifyFunction();
 void changeFunction(char ,int ,int);
+void displayPlot();
 
 int main()
 {
@@ -12,28 +13,20 @@ int main()
 
     printf("Enter rows and column: \n");
     scanf("%d %d", &row, &col);
-    printf("==========\n");
+    printf("\n");
     printf("Enter plot: \n");
-    for(i=0;i<row;i++)
-        for(j=0;j<col;j++)
-            scanf(" %c", &plot[i][j]);
-    printf("==========\n");
-    printf("Plot before modification: \n");
     for(i=0;i<row;i++)
     {
         for(j=0;j<col;j++)
-            printf("%c", plot[i][j]);
-        printf("\n");
+            scanf(" %c", &plot[i][j]);
     }
+    printf("==========\n");
+    printf("Plot before modification: \n");
+    displayPlot();
     modifyFunction();
     printf("==========\n");
     printf("Plot after modification: \n");
-    for(i=0;i<row;i++)
-    {
-        for(j=0;j<col;j++)
-            printf("%c", plot[i][j]);
-        printf("\n");
-    }
+    displayPlot();
     printf("==========\n");
     return 0;
 }
@@ -42,28 +35,23 @@ int main()
 void modifyFunction(int i, int j)
 {
     char nc='A';
-    //printf("In modification function\n");
     for(i=0; i<row; i++)
     {
         for(j=0; j<col; j++)
         {
-            //printf("%d\n", plot[i][j]);
             if(plot[i][j]=='@')
             {
-                //printf("Calling change.\n");
                 changeFunction(nc, i, j);
                 nc++;
             }
         }
     }
-    //printf("Exiting modification function\n");
+    printf("Total land count = %d\n", nc-65);
 }
 
 void changeFunction(char c, int i, int j)
 {
-    //printf("------------\n");
     plot[i][j]=c;
-    //printf("i=%d j=%d a[i][j]=%c\n", i, j, plot[i][j]);
     if(j-1>=0 && plot[i][j-1]=='@')
         changeFunction(c, i, j-1);
     if((j+1)<col && plot[i][j+1]=='@')
@@ -73,7 +61,16 @@ void changeFunction(char c, int i, int j)
     return;
 }
 
-
+void displayPlot()
+{
+    int i, j;
+    for(i=0;i<row;i++)
+    {
+        for(j=0;j<col;j++)
+            printf("%c", plot[i][j]);
+        printf("\n");
+    }
+}
 /*     ----input set 1
 Enter rows and column:
 4
@@ -121,6 +118,7 @@ Plot before modification:
 .....@.
 @@@..@.
 .@@.@@@
+Total land count = 6
 ==========
 Plot after modification:
 A..BB.C
